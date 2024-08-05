a076b842955d97a07bcbaf1e31643f9944538b73
Pete Stenger <pete@stenger.io>
2024-08-04 20:10:41 -0400

    switch to yarn lockfile

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,7 +1,4 @@
 import Card from "../components/Card/Card";
-import pwny8 from '../stories/assets/pwny8.svg';
-import sigaida from '../stories/assets/sigaidalogo.png';
-import sigmobile from '../stories/assets/sigmobile.png';
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 export default {
@@ -17,7 +14,6 @@ SIGPwny.args = {
     description: "A friendly but elite club focused on cybersecurity. They host weekly learning meetings and do CTFs and cutting-edge cybersecurity research.",
     link1: "https://sigpwny.com/",
     link2: "https://sigpwny.com/discord",
-    img: pwny8,
     linktext1: "Website",
     linktext2: "Discord"
 }
@@ -27,7 +23,6 @@ SIGAIDA.args = {
     description: "We are the premier data science organization at the University of Illinois.",
     link1: "https://aida.acm.illinois.edu/",
     link2: "https://discord.gg/GEMh8umbe7",
-    img: sigaida,
     linktext1: "Website",
     linktext2: "Discord"
 }
@@ -37,7 +32,6 @@ SIGMobile.args = {
     description: "Special Interest Group for Mobile Development.",
     link1: "http://github.com/SIGMobileUIUC",
     link2: "https://discord.gg/WN7pRp5PmW",
-    img: sigmobile,
     linktext1: "Github",
     linktext2: "Discord"
 }

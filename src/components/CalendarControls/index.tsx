3910c5ce58c36bb2173777628b4f5cd626008f12
Pete Stenger <pete@stenger.io>
2024-08-29 12:59:40 -0500

    WIP

--- src/components/CalendarControls/index.tsx
+++ src/components/CalendarControls/index.tsx
@@ -47,16 +47,16 @@ export default function CalendarControls({currDisplayDate, updateDisplayDate, cu
     return (
     <>
     <div className="grid lg:grid-cols-12 grid-cols-8 w-full">
-      <div className="flex col-span-4 lg:col-span-5">
-        <h3>{extractMonthAndYear(currDisplayDate)}</h3>
+      <div className="flex col-span-3 md:col-span-4 lg:col-span-5 items-center">
+        <div className="text-xl font-bold">{currView == Views.DAY ? getCurrentDate(currDisplayDate) : extractMonthAndYear(currDisplayDate)}</div>
       </div>
-        <div className="flex items-center ml-4 gap-x-4 col-span-4 lg:col-span-6">
-          <Button onPress={()=> {resetDate()}} variant="bordered" className="border-surface-000 border-1 bg-primary text-white hover:cursor-pointer">Today</Button>
+        <div className="flex items-center ml-4 gap-x-4 col-span-5 md:col-span-4 lg:col-span-6">
+          <Button onPress={()=> {resetDate()}} variant="bordered" className="border-surface-000 border-1 bg-primary text-white hover:cursor-pointer hidden md:block">Today</Button>
           <ButtonGroup>
               <Button isIconOnly onPress={() => {changeDate(-1, currView)}} variant="bordered" className="border-surface-000 border-1 bg-primary text-white hover:cursor-pointer "><FaArrowLeft /></Button>
               <Button disabled={nextDisabled} isIconOnly onPress={() => {changeDate(1, currView)}} variant="bordered" className="border-surface-000 border-1 bg-primary text-white hover:cursor-pointer" ><FaArrowRight /></Button>
           </ButtonGroup>
-          <div className="hidden lg:block">
+          <div>
             <Dropdown>
               <DropdownTrigger>
                 <Button 
@@ -70,7 +70,7 @@ export default function CalendarControls({currDisplayDate, updateDisplayDate, cu
                 onAction={(key) => updateCurrView(key as View)}
               >
                 <DropdownItem key={Views.DAY}>Day View</DropdownItem>
-                <DropdownItem key={Views.WEEK}>Week View</DropdownItem>
+                <DropdownItem key={Views.WEEK} className="hidden md:block">Week View</DropdownItem>
                 <DropdownItem key={Views.MONTH}>Month View (default)</DropdownItem>
                 <DropdownItem key={Views.AGENDA}>Agenda View</DropdownItem>
               </DropdownMenu>
@@ -78,7 +78,6 @@ export default function CalendarControls({currDisplayDate, updateDisplayDate, cu
           </div>
         </div>
     </div>
-    <a style={{paddingRight: '3vw'}}>{currView == Views.DAY ? getCurrentDate(currDisplayDate): null}</a> 
     </>
     )
 }
--- src/components/CalendarEventDetail/CalendarEventDetail.tsx
+++ src/components/CalendarEventDetail/CalendarEventDetail.tsx
@@ -53,7 +53,7 @@ function CalendarEventDetail({
             ? `${moment(start).format('h:mm A')} - ${moment(end).format('h:mm A z')} ${timezoneAbbr}` 
             : start && `${moment(start).format('h:mm A z')} ${timezoneAbbr}`;
         return (
-                <div className='bg-surface-000 break-words border-2 border-acmdark border-opacity-10 mt-2 border-t-transparent border rounded-2xl'>
+                <div className='flex flex-col flex-grow bg-surface-000 break-words border-2 border-acmdark border-opacity-10 mt-2 border-t-transparent border rounded-2xl'>
                     <div className='text-xl w-full text-surface-000 text-center bg-acmdark rounded-t-2xl pb-2 pt-2 font-semibold'>Event Information</div>
                     <div className='p-5'>
                         <h4 className='flex justify-between'>{title} {host && getOrganizationImage(host, 'w-auto h-8')} </h4>
@@ -64,25 +64,24 @@ function CalendarEventDetail({
                         <div className='flex flex-row items-center gap-2'>
                             <FaRegCalendarAlt className="shrink-0"/> {calendar}
                         </div>
-                        <div className='flex flex-row justify-between'>
+                        <div className='flex flex-col md:flex-row justify-between'>
                         {host && (
                             <div className='flex flex-row items-center gap-2 mb-2'>
                                 <FaUserAlt className="shrink-0" /> {host}
                             </div>
                         )}
-                        <div className='flex flex-row'>
-
-                        {info?.links.map(({ link, text }) => (
-                            <a
-                            key={text}
-                            className="font-medium p-1 rounded-md text-primary hover:bg-surface-100 transition-all"
-                            href={link}
-                            target="_blank"
-                            rel="noopener noreferrer">
-                            {text}
-                        </a>)
-                        )}
-                        </div>
+                            <div className='flex flex-row -ml-1 md:ml-0'>
+                            {info?.links.map(({ link, text }) => (
+                                <a
+                                key={text}
+                                className="font-medium px-1 rounded-md text-primary hover:bg-surface-100 transition-all"
+                                href={link}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                {text}
+                            </a>)
+                            )}
+                            </div>
                         </div>
                         <p>{description}</p>
                         {paidEventId && (

14439210d2a421a8e1da88cebcb561cf40220a59
anandani4136 <ra7353@hotmail.com>
2024-09-05 10:28:34 -0500

    Add Membership Guide Link

--- src/app/(membership)/membership/page.tsx
+++ src/app/(membership)/membership/page.tsx
@@ -130,6 +130,9 @@ const Payment = () => {
               Becoming a Lifetime <b>Paid Member</b> not only sustains the continued growth of our communities but also
               comes with perks such as swipe access, free printing, priority access to our computing resources, etc.
             </p>
+            <a className="text-primary"  href="https://docs.google.com/document/d/1ZtPo5uCtlwGY_4tmWebpWoEB6IM-I4J7a_HA1ZK2FBI/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+              ACM@UIUC Paid Member Guide
+            </a>
             <Input
               value={netId}
               onValueChange={setNetId}

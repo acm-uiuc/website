3bc7c1330a53bc0bb4fc0d8bbd5f94b53eb5ad97
anandani4136 <ra7353@hotmail.com>
2025-01-23 14:33:55 -0600

    Make the tables bigger

--- src/app/(open-house)/data/tables.ts
+++ src/app/(open-house)/data/tables.ts
@@ -12,125 +12,125 @@ const tables: Table[] = [
       top: 82,
       left: 65,
       width: 10,
-      height: 7
+      height: 8
     },
     {
       id: 2,
       top: 82,
       left: 55,
       width: 10,
-      height: 7
+      height: 8
     },
     {
       id: 3,
       top: 82,
       left: 60,
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 4,
       top: 82,  
       left: 45,  
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 5,
       top: 82,  
       left: 30,  
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 6,
       top: 82,
       left: 15,
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 7,
-      top: 63,
+      top: 62,
       left: 61,
       width: 10,
-      height: 7
+      height: 8
     },
     {
       id: 8,
-      top: 63,
-      left: 58,
+      top: 62,
+      left: 57,
       width: 13,
-      height: 7
+      height: 8
     },
     {
       id: 9,
-      top: 63,
+      top: 62,
       left: 44,
-      width: 14,
-      height: 7
+      width: 13,
+      height: 8
     },
     {
       id: 10,
-      top: 63,
+      top: 62,
       left: 31,
       width: 13,
-      height: 7
+      height: 8
     },
     {
       id: 11,
       top: 69,
       left: 8,
-      width: 6,
+      width: 7,
       height: 12
     },
     {
       id: 12,
       top: 57,
       left: 25,
-      width: 6,
+      width: 7,
       height: 13
     },
     {
       id: 13,
       top: 66,  
       left: 8,  
-      width: 6,
+      width: 7,
       height: 15 
     },
     {
       id: 14,
       top: 50,
-      left: 25,
-      width: 6,
+      left: 24,
+      width: 7,
       height: 20
     },
     {
       id: 15,
       top: 50,
       left: 8,
-      width: 6,
+      width: 7,
       height: 15
     },
     {
       id: 16,
       top: 30,
-      left: 25,
-      width: 6,
+      left: 24,
+      width: 7,
       height: 20
     },
     {
       id: 17,
       top: 34,  
       left: 8,  
-      width: 6,
+      width: 7,
       height: 15 
     },
     {
       id: 18,
       top: 18,  
       left: 8,  
-      width: 6,
+      width: 7,
       height: 15 
     },
     {
@@ -138,125 +138,125 @@ const tables: Table[] = [
       top: 9,  
       left: 15,  
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 20,
       top: 9,
       left: 29,
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 21,
       top: 9,
       left: 43,
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 22,
       top: 9,
       left: 57,
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 23,
       top: 9,
       left: 71,
       width: 14,
-      height: 7
+      height: 8
     },
     {
       id: 24,
       top: 9,  
       left: 65, 
       width: 10,
-      height: 7
+      height: 8
     },
     {
       id: 25,
       top: 9,  
       left: 75,  
       width: 10,
-      height: 7 
+      height: 8 
     },
     {
       id: 26,
       top: 30,
       left: 31,
       width: 13,
-      height: 7
+      height: 8
     },
     {
       id: 27,
       top: 30,
       left: 44,
-      width: 14,
-      height: 7
+      width: 13,
+      height: 8
     },
     {
       id: 28,
       top: 30,
-      left: 58,
+      left: 57,
       width: 13,
-      height: 7
+      height: 8
     },
     {
       id: 29,
       top: 30,
       left: 61,
       width: 10,
-      height: 7
+      height: 8
     },
     {
       id: 30,
       top: 17,  
       left: 87,  
-      width: 6,
+      width: 7,
       height: 16
     },
     {
       id: 31,
       top: 34,  
       left: 87,  
-      width: 6,
+      width: 7,
       height: 16
     },
     {
       id: 32,
       top: 51,  
       left: 87,  
-      width: 6,
+      width: 7,
       height: 16 
     },
     {
       id: 33,
       top: 56,  
       left: 87,  
-      width: 6,
+      width: 7,
       height: 12 
     },
     {
       id: 34,
       top: 30,
-      left: 71,
-      width: 6,
+      left: 70,
+      width: 7,
       height: 20
     },
     {
       id: 35,
       top: 50,
-      left: 71,
-      width: 6,
+      left: 70,
+      width: 7,
       height: 20
     },
     {
       id: 36,
       top: 57,
       left: 71,
-      width: 6,
+      width: 7,
       height: 13
     }
 ]

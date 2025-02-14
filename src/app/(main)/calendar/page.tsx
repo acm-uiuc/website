bb45322007241c432fbb29708c45d6e59e3ef30b
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2025-02-13 23:34:05 -0600

    Set size ratio between CalendarControls and dropdown+search using grid: At XL: left-5/8, right-3/8 At LG: left-2/3, right-1/3
    Set size ratio between dropdown menu and search bar:
    dropdown-2/5, search bar-3/5

--- src/app/(main)/calendar/page.tsx
+++ src/app/(main)/calendar/page.tsx
@@ -70,37 +70,41 @@ const Calendar = () => {
     <Suspense>
       <section className="container">
         <h1 className='mt-0 pt-0 mb-4'>Our Events</h1>
-        <div className="flex">
-          <CalendarControls currDisplayDate={displayDate} updateDisplayDate={setDisplayDate} currView={view} updateCurrView={setView} />
-          <div className="hidden xl:flex">
-            <select
-              value={hostFilter}
-              onChange={(e) => {
-                setHostFilter(e.target.value);
-                const baseURL = window ? window.location.origin : 'https://acm.illinois.edu';
-                if (e.target.value === "") {
-                  window.history.replaceState(null, '', baseURL + `/calendar`);
-                } else {
-                  window.history.replaceState(null, '', baseURL + `/calendar?host=${e.target.value.replaceAll(" ", "+")}`);
-
-                }
-              }}
-              className="border-2 border-gray-300 rounded-md mr-2 px-2" // Styling for the dropdown
-            >
-              <option value="">Filter by host</option>
-              {validOrganizations.map((org) => (
-                <option key={org} value={org}>{org}</option>
-              ))}
-            </select>
+        <div className="grid lg:grid-cols-3 xl:grid-cols-8 w-full gap-4">
+          <div className='flex lg:col-span-2 xl:col-span-5'>
+            <CalendarControls currDisplayDate={displayDate} updateDisplayDate={setDisplayDate} currView={view} updateCurrView={setView} />
           </div>
-          <div className="hidden lg:flex">
-            <input
-              type="text"
-              placeholder="Search events"
-              value={filter}
-              onChange={handleFilterChange}
-              className="ml-4 px-2 border-2 border-gray-300 focus:border-blue-500 rounded-md" // Updated styling
-            />
+          <div className='flex lg:col-span-1 xl:col-span-3 w-full gap-4 justify-end'>
+            <div className="hidden xl:flex xl:w-2/5">
+              <select
+                value={hostFilter}
+                onChange={(e) => {
+                  setHostFilter(e.target.value);
+                  const baseURL = window ? window.location.origin : 'https://acm.illinois.edu';
+                  if (e.target.value === "") {
+                    window.history.replaceState(null, '', baseURL + `/calendar`);
+                  } else {
+                    window.history.replaceState(null, '', baseURL + `/calendar?host=${e.target.value.replaceAll(" ", "+")}`);
+
+                  }
+                }}
+                className="px-2 border-2 border-gray-300 rounded-md w-full" // Styling for the dropdown
+              >
+                <option value="">Filter by host</option>
+                {validOrganizations.map((org) => (
+                  <option key={org} value={org}>{org}</option>
+                ))}
+              </select>
+            </div>
+            <div className="hidden lg:flex xl:w-3/5">
+              <input
+                type="text"
+                placeholder="Search events"
+                value={filter}
+                onChange={handleFilterChange}
+                className="px-2 border-2 border-gray-300 focus:border-blue-500 rounded-md w-full" // Updated styling
+              />
+            </div>
           </div>
         </div>
         <div className='grid justify-between pb-10 gap-4 grid-cols-8'>

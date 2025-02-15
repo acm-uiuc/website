6db4d4f582f1b152c992dad0e3736a1e7530e245
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2025-02-15 12:55:00 -0600

    reduced gap under placeholder text of EventDetail

--- src/app/(main)/calendar/page.tsx
+++ src/app/(main)/calendar/page.tsx
@@ -70,7 +70,7 @@ const Calendar = () => {
     <Suspense>
       <section className="container">
         <h1 className='mt-0 pt-0 mb-4'>Our Events</h1>
-        <div className="grid lg:grid-cols-3 xl:grid-cols-8 w-full gap-6">
+        <div className="grid lg:grid-cols-3 xl:grid-cols-8 w-full gapx-6">
           <div className='flex lg:col-span-2 xl:col-span-5'>
             <CalendarControls currDisplayDate={displayDate} updateDisplayDate={setDisplayDate} currView={view} updateCurrView={setView} />
           </div>

16143ab343fe3d461a73c97c87f634d7990c499d
jlevine18 <jacoblevine18@gmail.com>
2025-03-08 15:21:22 -0600

    add json content type

--- src/app/(membership)/check-membership/page.tsx
+++ src/app/(membership)/check-membership/page.tsx
@@ -87,6 +87,7 @@ const Payment = () => {
     try {
       await axios.post(`${walletApiBaseUrl}/api/v1/mobileWallet/membership`, null, {
         params: { email: `${netId}@illinois.edu` }
+        headers: { Content-Type: 'application/json' }
       });
       setIsWalletLoading(false);
     } catch (error: any) {

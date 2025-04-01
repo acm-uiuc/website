a09b328eea1e8424a74695c8762388f4679f2e5c
Dev Singh <dev@devksingh.com>
2025-03-31 21:22:16 -0500

    email regex part 2

--- src/app/(membership)/merch/page.tsx
+++ src/app/(membership)/merch/page.tsx
@@ -152,12 +152,9 @@ const MerchItem = () => {
   };
 
   const validateEmail = (value: string) => {
-    return value
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+  }
+
   const changeSize = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setSize(e ? e.target ? e.target.value : "" : "");
   };

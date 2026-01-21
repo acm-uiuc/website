c35e7bef4f89e71120e6168d5426f03aeded89bb
Jake Levine <jlevine4@illinois.edu>
2026-01-20 23:30:03 -0600

    fix moreinfourl button on main page

--- src/components/Card/EventCard.tsx
+++ src/components/Card/EventCard.tsx
@@ -43,7 +43,7 @@ export default function EventCard({ event }: EventProps) {
     repeats,
     paidEventId,
     host,
-    mapLinkId,
+    moreInfoURL,
     location,
   } = event || {};
   const link = event ? getEventURL(event) : '';
@@ -92,10 +92,10 @@ export default function EventCard({ event }: EventProps) {
           </a>
         ) : null}
 
-        {mapLinkId ? (
+        {moreInfoURL ? (
           <a
             className="inline-flex flex-row grow-0 items-center gap-2 px-4 py-2 text-white rounded-2xl bg-primary hover:bg-secondary transition-all"
-            href={mapLinkId}
+            href={moreInfoURL}
             target="_blank"
             rel="noopener noreferrer"
           >

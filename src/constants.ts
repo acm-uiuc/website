486076bd188f43dd0df6c3ffbd285cfba67bc71b
Dev Singh <dev@devksingh.com>
2026-02-05 03:28:58 -0600

    A terrible typing effect

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,18 @@
 export const mailingListSubscriptionUrl =
   'https://listmonk.acm.illinois.edu/subscription/form';
+
+export const typewriterMessages = [
+  'AI',
+  'Web Development',
+  'Cybersecurity',
+  'GNU/Linux',
+  'Data Science',
+  'Human-Computer Interaction',
+  'Game Development',
+  'Cloud Computing',
+  'Mobile',
+  'Robotics',
+  'Computer Music',
+  'Econometrics',
+  'Everything CS',
+];
